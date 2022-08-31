4d5b879b087540f0579e53937766917b3743300c
Wen-Heng (Jack) Chung <whchung@gmail.com>
2022-08-31 14:39:00 +0000

    test VECTOR_ADD_ISA.

--- src/core/runtime/hsa_ven_amd_loader.cpp
+++ src/core/runtime/hsa_ven_amd_loader.cpp
@@ -142,7 +142,7 @@ hsa_status_t HSA_API hsa_ven_amd_experiment_get_pm4(
     reinterpret_cast<uint32_t*>(pm4_b_buf)[i] = 0x3C003C00; // 1.0 (half) / 1.0 (half)
   }
 
-  memcpy(pm4_isa_buf, VECTOR_SET_ISA, sizeof(VECTOR_SET_ISA));
+  memcpy(pm4_isa_buf, VECTOR_ADD_ISA, sizeof(VECTOR_ADD_ISA));
  
   // Parameters need to be set:
   // - ISA address.
